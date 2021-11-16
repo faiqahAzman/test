2f1850ca9c8775a900e0525dbc08b06485a5bcae
faiqahAzman <u2001400@siswa.um.edu.my>
2021-11-16 11:27:47 +0800

    Initial commit

---
+++ JavaApplication15/src/javaapplication15/JavaApplication15.java
@@ -0,0 +1,13 @@
+
+package javaapplication15;
+
+
+public class JavaApplication15 {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+    }
+    
+}
