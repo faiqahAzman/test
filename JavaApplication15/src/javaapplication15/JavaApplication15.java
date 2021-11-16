2359cc9b7338fc1084fb61303fa3ab60657657db
faiqahAzman <u2001400@siswa.um.edu.my>
2021-11-16 12:35:31 +0800

    Initial commit

--- JavaApplication15/src/javaapplication15/JavaApplication15.java
+++ JavaApplication15/src/javaapplication15/JavaApplication15.java
@@ -8,6 +8,8 @@ public class JavaApplication15 {
     public static void main(String[] args) {
         
         System.out.println("Hello World!");
+        int x =2;
+        System.out.println(x);
     }
     
 }
